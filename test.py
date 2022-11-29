8b8866bbaa622341a140b987a45bc90b4ede5815
Bartosz Ziolkowski <98161981+ziolkowski-bartosz@users.noreply.github.com>
2022-11-29 20:21:14 +0100

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ END_DATE = '2022-04-30'
 
 
 class Test(unittest.TestCase):
-    def run_test(self):
+    def runTest(self):
         app.run(countries_csv_name='a3-countries.csv', countries=['Denmark','Sweden','Norway'], sample_ratio= SAMPLE_RATIO, start_date= START_DATE, end_date= END_DATE)
 
 
